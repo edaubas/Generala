e6f7b4092d5b80ef789430a809bc34592513dc1a
Emiliano <pope_ad@hotmail.com>
2021-07-13 11:21:38 -0300

    Updates

---
+++ precache-manifest.af7b10c733072a127444c2acdaeb03b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40dd3c54081bc0f2d688482d465a17a",
+    "url": "/Generala/index.html"
+  },
+  {
+    "revision": "5cb94b2bd1cc188ebe46",
+    "url": "/Generala/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0a47b6f88fcc6a175d8d",
+    "url": "/Generala/static/css/main.1651d728.chunk.css"
+  },
+  {
+    "revision": "5cb94b2bd1cc188ebe46",
+    "url": "/Generala/static/js/2.49bdccf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Generala/static/js/2.49bdccf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a47b6f88fcc6a175d8d",
+    "url": "/Generala/static/js/main.c2a60991.chunk.js"
+  },
+  {
+    "revision": "6de4d5092780de1b08e3",
+    "url": "/Generala/static/js/runtime-main.d940f7ba.js"
+  }
+]);
